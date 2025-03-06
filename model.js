ed2d053bdb6d9a6724904b340c3e5b90b81a9379
Viss666 <vistral9546@gmail.com>
2025-03-06 12:22:30 -0700

    added mongoose connect to env

--- model.js
+++ model.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 const playerSchema = new mongoose.Schema({
   playerName: {
     type: String,
